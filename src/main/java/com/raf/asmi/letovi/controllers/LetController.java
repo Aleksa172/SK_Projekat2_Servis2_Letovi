36ea2c3a6755501bab4c1568260452e4055d47a9
Aleksa Smiljanic <asmiljanic16@raf.rs>
2021-01-03 18:09:17 +0100

    Razradjeno brisanje letova

--- src/main/java/com/raf/asmi/letovi/controllers/LetController.java
+++ src/main/java/com/raf/asmi/letovi/controllers/LetController.java
@@ -3,7 +3,20 @@ package com.raf.asmi.letovi.controllers;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -172,6 +185,45 @@ public class LetController {
 			letRepository.delete(let);
 		}
 		
+		// Ovo bi zapravo trebalo da radi servis 3 zato sto on zna ko je kupio kartu
+		// this.sendCancellationEmail(destination, let);
+		
 		return "ok";
+	}
+	
+	
+	private void sendCancellationEmail(String destination, Let let) throws AddressException, MessagingException {
+		Properties prop = new Properties();
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.starttls.enable", "true");
+		prop.put("mail.smtp.host", "smtp.mailtrap.io");
+		prop.put("mail.smtp.port", "25");
+		prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+		
+		Session session = Session.getInstance(prop, new Authenticator() {
+		    @Override
+		    protected PasswordAuthentication getPasswordAuthentication() {
+		        return new PasswordAuthentication("acdcb8aae49500", "f9513570a4444f");
+		    }
+		});
+		
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress("noreply@skprojekat2.com"));
+		message.setRecipients(
+		  Message.RecipientType.TO, InternetAddress.parse(destination));
+		message.setSubject("Mail Subject");
+
+		String msg = "Vas let "+let.getPocetnaDestinacija()+" - "+let.getKrajnjaDestinacija()+" je nazalmost otkazan <br/>" +
+			"Uskoro ce vam biti povracen novac.";
+
+		MimeBodyPart mimeBodyPart = new MimeBodyPart();
+		mimeBodyPart.setContent(msg, "text/html");
+
+		Multipart multipart = new MimeMultipart();
+		multipart.addBodyPart(mimeBodyPart);
+
+		message.setContent(multipart);
+
+		Transport.send(message);
 	}
 }
